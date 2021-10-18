73c76cf97ea540d7b3b0b966f51b73ede8f7f070
Yasemin Khertek <hertek.y@yandex.ru>
2021-10-18 19:37:28 +0300

    Основное задание

--- client.py
+++ client.py
@@ -1,16 +1,69 @@
-import socket
-from time import sleep
+# Импортировать пакет сокета
+import socket, threading
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+ # Создать клиентский объект
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+ # Целевой хост
+host = input('Целевой IP-адрес входа: ')
+ 
+while True:
+    name = input ('Пожалуйста, введите личный ник (1<длина<10): ')
+    if 1<len(name)<10:
+        break
+ 
+ # Порт назначения
+port = 9090
+ 
+ # Подключить клиента
+client.connect((host, port))
+print ('-' * 5+ 'подключился к серверу' + '-' * 5)
+print ('-' * 5 + 'Enter, чтобы закрыть соединение с сервером' + '-' * 5)
+ 
+ 
+def outdatas():
+    while True:
+ 
+                 # Введите информацию, которая будет отправлена ​​на сервер
+        outdata = input('')
+        print()
+        if outdata=='enter':
+            break
+                 # Отправить на сервер
+        client.send(f'{name}: {outdata}'.encode('utf-8'))
+        print('%s:%s'% (name, outdata))
+ 
+ 
+def indatas():
+ 
+    while True:
+                 # Принимаем информацию с сервера
+        try:
+            indata = client.recv(1024)
+ 
+                 # Закодировать полученную информацию
+            print(indata.decode('utf-8'))
+        except:
+            print('--Отключение--')
+            break
+ 
+ 
+ # Создать многопоточность
+ # Установить получение информации, объект потока
+t1 = threading.Thread(target=indatas, name='input')
+ 
+ # Создание выходной информации, объект потокаenter
+t2 = threading.Thread(target=outdatas, name='out')
+ 
+ # Начать многопоточность
+t1.start()
+t2.start()
+ 
+ # Заблокировать поток, основной поток не может завершиться, пока не завершится выполнение дочернего потока.
+# t1.join()
+t2.join()
+ 
+ # Закрыть соединение
+print ('-' * 5 + 'сервер отключен' + '-' * 5)
+client.close()
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
-
-data = sock.recv(1024)
-
-sock.close()
-
-print(data.decode())
--- server.py
+++ server.py
@@ -1,20 +1,91 @@
-import socket
+import socket, threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-msg = ''
+host = socket.gethostname()
+print(f'Сервер: {host}')
+server.bind((host, 9090))
+server.listen(5)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+print ('Enter для выхода с сервера')
 
-print(msg)
+ # Создайте список клиентов
+clients = list()
+ # Хранить клиентов, которые создали потоки
+end = list()
 
-conn.close()
+ # Блокировка ожидания подключения клиента, возврата объекта подключения и адреса косвенного объекта
+def accept():
+    while True:
+        client, addr = server.accept()
+        clients.append(client)
+        print ('-' * 2 + f'сервер подключен через {addr}: текущее количество подключений: -- {len (clients)}' + '-' * 2, end = '')
+
+
+def recv_data(client):
+    while True:
+                 # Принимаем информацию от клиента
+        try:
+            indata = client.recv(1024)
+        except:
+            clients.remove(client)
+            end.remove(client)
+            print ('-' * 2 + f'Сервер отключен: текущее количество подключений: -- {len (clients)}' + '-' * 2, end = '')
+            break
+        print(indata.decode('utf-8'))
+        for clien in clients:
+            # Перенаправить информацию от клиента и отправить ее другим клиентам
+            if clien != client:
+                clien.send(indata)
+ 
+def outdatas():
+    while True:
+        # Введите информацию, которая будет предоставлена ​​клиенту
+        print('')
+        outdata = input('')
+        print()
+        if outdata=='enter':
+            break
+            print ('Отправить всем: % s'% outdata)
+        # Отправлять информацию каждому клиенту
+        for client in clients:
+            client.send (f"Сервер: {outdata}". encode ('utf-8)'))
+ 
+ 
+def indatas():
+    while True:
+                 # Выполните цикл подключенных клиентов и создайте соответствующий поток
+        for clien in clients:
+            # Если поток уже существует, пропустить
+            if clien in end:
+                continue
+            index = threading.Thread(target = recv_data,args = (clien,))
+            index.start()
+            end.append(clien)
+ 
+ 
+ # Создать многопоточность
+ # Создать получающую информацию, объект потока
+t1 = threading.Thread(target = indatas,name = 'input')
+t1.start()
+ 
+ # Создать отправляемое сообщение, объект потока
+ 
+t2 = threading.Thread(target = outdatas, name= 'out')
+t2.start()
+ 
+ # Ожидание подключения клиента, объект потока
+ 
+t3 = threading.Thread(target = accept(),name = 'accept')
+t3.start()
+ 
+ # Блокировать округ, пока подпоток не будет завершен, и основной поток не может закончиться
+# t1.join()
+t2.join()
+ 
+ # Выключите все серверы
+for client in clients:
+    client.close()
+print ('-' * 2 + 'сервер отключен' + '-' * 2)
+
+
