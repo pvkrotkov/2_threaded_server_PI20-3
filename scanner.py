3a4a3be21fc5569b64f52895a23721039cbd1fef
Artem Vyrodov <dizpatcher@outlook.com>
2021-10-19 00:47:56 +0300

    timeout debugging

--- scanner.py
+++ scanner.py
@@ -4,57 +4,47 @@ import sys
 from threading import Thread
 
 
-STEP = 66 # каждому потоку будем отводить 66 портов
-#         66 - чтобы обработать тысячу портов 15 потоками - рекоммендованное число
-open_ports = []
+PORT_AMOUNT = 1000 # сколько портов нужно проверить
+THR_AMOUNT = PORT_AMOUNT # количество потоков
 threads = []
+open_ports = []
 address = input('Введите адрес для проверки портов: ')
 
-
-def scan(address, ports):
-    global STEP
-    for port in ports:
-        #print(f'Сканируем порт №{port}...')
-        try:
-            socket.socket().connect((address, port))
-            global open_ports
-            open_ports.append(port)
-            #print(f'ОТКРЫТО! - порт №{port}!!!')
-        except:
-            #print(f'Порт №{port} закрыт.')
-            continue
-    #print(f'Поток №{ports[0]//STEP+1} работу завершил!')
-
-
 # настройка прогресс бара
-toolbar_width = 40
+toolbar_width = THR_AMOUNT
 sys.stdout.write("[%s]" % (" " * toolbar_width))
 sys.stdout.flush()
 sys.stdout.write("\b" * (toolbar_width+1))
 
 
-for port in range(1,1001,STEP):
-    threads.append(Thread(target = scan, args=(address, [numb for numb in range(port, port+STEP)]))) # помещаем поток в список, чтобы отслеживать их завершение
-    threads[port//STEP].start() # запускаем поток
-
-
-# внешний цикл для работы прогресс-бара, внутренний - алгоритм
-for i in range(toolbar_width):
-    time.sleep(0.1)
-
-    # ожидаем завершения работы всех потоков
-    while len(threads)>0:
-        for i in range(len(threads)-1,-1,-1):
-            if not threads[i].is_alive():
-                threads.remove(threads[i])
+def scan(address, ports):
+    for port in ports:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            sock.connect((address, port))
+            global open_ports
+            open_ports.append(port)
+        except:
+            continue
+        finally:
+            sock.close()
 
     sys.stdout.write("-")
+    time.sleep(0.001)
     sys.stdout.flush()
 
+
+for i in range(1,THR_AMOUNT+1):
+    infinum = PORT_AMOUNT*(i-1)//THR_AMOUNT # первый порт для потока №i
+    suprenum = i*PORT_AMOUNT//THR_AMOUNT # последний порт для потока №i
+    threads.append(Thread(target=scan, args=(address, [port for port in range(infinum, suprenum)])))
+
+[t.start() for t in threads] # запуск всех потоков
+[t.join() for t in threads] # дожидаемся завершения всех потоков
 sys.stdout.write("\n")
 
 print('Открытые порты: ', end='')
 for port in sorted(open_ports):
-    print(port, end=' ,')
+    print(port, end=' ')
 
-print(f'.\nВсего: {len(open_ports)}.')
+print(f'\nВсего: {len(open_ports)}.')
