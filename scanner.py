620a796c2af5bc389ee54d6807d69890d7552921
Yasemin Khertek <hertek.y@yandex.ru>
2021-10-21 16:11:18 +0300

    Сканер портов

--- scanner.py
+++ scanner.py
@@ -1,16 +1,34 @@
 import socket, threading, time
 from progress.bar import IncrementalBar #необходимо установить модуль Progress (pip install progress)
 
+ports=[]
 # функция для сканирования порта
 def scan_p(ip,port):
+    global ports
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(0.1)
     try:
         sock.connect((ip,port))
-        print(f" ------ Порт {port} открыт ------")
         sock.close()
+        ports.append(port)
     except:
         pass
+
+def print_port():
+                 # Выполните цикл подключенных клиентов и создайте соответствующий поток
+        for p in range(i):
+            # Если поток уже существует, пропустить
+            if p in ports:
+                continue
+            index = threading.Thread(target = scan_p,args = (ip,p))
+            index.start()
+        if len(ports)>=1:
+            print('\n',f"------ Количество октрытых портов {len(ports)} ------")
+            print(' ------ Порты:')
+            for port in ports:
+                print(f' --------- {port}')
+        else:
+            print('Нет открытых портов')
 
 #N = 2**16 - 1 - максимальное количество возможных портов
 
@@ -25,6 +43,9 @@ for port in range(i):
 
     bar.next()#запуск progress bar
     time.sleep(0.1)
-    
+
+thread2 = threading.Thread(target=print_port, name='open')
+thread2.start()
+thread2.join()    
+
 bar.finish()
-    
