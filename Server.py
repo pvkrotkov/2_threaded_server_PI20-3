8dfe6e28786d5b83c09bcf143e5e7e0d0f7b804c
Nian <76498608+AntonY4ZerO@users.noreply.github.com>
2021-10-21 15:24:43 +0300

    Add files via upload

---
+++ Server.py
@@ -0,0 +1,17 @@
+import socket
+
+sock = socket.socket()
+sock.bind(('', 6666))
+sock.listen(0)
+conn, addr = sock.accept()
+print(addr)
+
+msg = ''
+
+while msg !='exit':
+    data = conn.recv(1024)
+    msg = data.decode()
+    print(msg)
+    conn.send(data)
+
+conn.close()
