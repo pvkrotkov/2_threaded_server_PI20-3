13f4f04938eab674da38039e79c53bd5fdcd1935
Андрей <51806719+666Jonah666@users.noreply.github.com>
2021-10-27 13:50:35 +0000

    Scanner

--- scanner.py
+++ scanner.py
@@ -1,15 +1,24 @@
 import socket
-from threading import Thread
+import threading
+import pyprind
 
-N = 2**16 - 1
+N = 65536
 
-for port in range(1,100):
-    sock = socket.socket()
+Progress = pyprind.ProgBar(N)
+
+ip = '192.168.0.1'
+
+def scan_port(ip,port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(0.5)
     try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
+        connect = sock.connect((ip,port))
+        print(f'\nport {port} is open')
         sock.close()
+    except:
+        pass
+    finally:
+        Progress.update()
+
+if __name__ == '__main__':
+    [threading.Thread(target=scan_port, args=(ip, i)).start() for i in range(N)]
