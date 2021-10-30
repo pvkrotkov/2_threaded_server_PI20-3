3ee9700f09b7d5fc3ede694291079be025442a66
Sancho <90206600+PATAU4@users.noreply.github.com>
2021-10-30 14:15:13 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,46 @@
 import socket
-from time import sleep
+import threading
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+while True:
+    nickname = input ('Enter your nickname: ')
+    if 1<len(nickname)<20:
+        break
 
-data = sock.recv(1024)
+sock.connect(('localhost', 9091)) 
+
+
+def sending():
+    # Функция передачи data к серверу
+    sock.send(f'client {nickname} joined'.encode())
+    while True:
+        mes = (input())
+        sock.send(f'{nickname}: {mes}'.encode())
+        
+        if mes=='exit':
+            sock.close()   
+            break     
+        
+
+
+def receiving():
+    # Функция получения echo от сервера
+    while True:   
+        try:   
+            data = sock.recv(1024)
+            print(data.decode())
+        except:
+             print('Отключение')
+             break
+
+t1 = threading.Thread(target=sending, name='sending')
+t2 = threading.Thread(target=receiving, name='receiving')
+
+t1.start()
+t2.start()
+
+t2.join()
+
 
 sock.close()
-
-print(data.decode())
