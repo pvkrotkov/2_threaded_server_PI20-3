6a369195d9d4996c32a9cab729d1f45f1af4331d
Sancho <90206600+PATAU4@users.noreply.github.com>
2021-10-30 14:16:10 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,35 @@
 import socket
+import threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('localhost', 9091)) 
+sock.listen(5)  # максимальное количество клиентов 
 
-msg = ''
+
+
+
+def connect(sock):
+    # Функция принятия подключения клиента
+    while True:
+        conn, addr = sock.accept()
+        print(f'Client joined. Addres {addr}')
+        if conn is not None:
+            new_client(conn) 
+    
+def new_client(conn):
+    global dict_of_clients
+    name = conn.recv(1024)
+    name = name.decode()
+    conn.send('Echo from server'.encode())
+    output(conn)
+    
+def output(conn):
+    while True:
+        data = conn.recv(1024)  
+        print(data.decode())
+
 
 while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
-
-print(msg)
-
-conn.close()
+    threads = [threading.Thread(target=connect, args=[sock]) for _ in range(3)]
+    [thread.start() for thread in threads]
+    [thread.join() for thread in threads]
