2a75272a951e604e43bb2346a3b85657ea144daa
kate kirillova <76176114+gyul-i@users.noreply.github.com>
2021-11-03 15:49:38 +0300

    сервер

--- server.py
+++ server.py
@@ -1,20 +1,84 @@
-import socket
+import socket, threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-msg = ''
+#host = socket.gethostname()
+#print(f'Сервер: {host}')
+server.bind(('', 9090))
+server.listen(5)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+print ('exit для выхода с сервера')
 
-print(msg)
+clients = list() #Список клиентов
+end = list() #Клиенты, которые создали потоки
 
-conn.close()
+#Блокировка ожидания подключения клиента, возврата объекта подключения и адреса косвенного объекта
+def accept():
+    while True:
+        client, addr = server.accept()
+        clients.append(client)
+        print(f'сервер подключен через {addr}: текущее количество подключений: -- {len (clients)}', end = '')
+
+
+def recv_data(client):
+    while True:
+        #Принимаем информацию от клиента
+        try:
+            indata = client.recv(1024)
+        except:
+            clients.remove(client)
+            end.remove(client)
+            print(f'Сервер отключен: текущее количество подключений: -- {len (clients)}', end = '')
+            break
+        print(indata.decode('utf-8'))
+        for clien in clients:
+            # Перенаправить информацию от клиента и отправить ее другим клиентам
+            if clien != client:
+                clien.send(indata)
+ 
+def outdatas():
+    while True:
+        #Введите информацию, которая будет предоставлена клиенту
+        print('')
+        outdata = input('')
+        print()
+        if outdata == 'exit' or outdata == 'Exit':
+            break
+            print('Отправить всем: % s'% outdata)
+        # Отправлять информацию каждому клиенту
+        for client in clients:
+            client.send (f"Сервер: {outdata}". encode ('utf-8)'))
+ 
+ 
+def indatas():
+    while True:
+        #Выполните цикл подключенных клиентов и создайте соответствующий поток
+        for clien in clients:
+            # Если поток уже существует, пропустить
+            if clien in end:
+                continue
+            index = threading.Thread(target = recv_data,args = (clien,))
+            index.start()
+            end.append(clien)
+ 
+ 
+#Создать многопоточность
+
+t1 = threading.Thread(target = indatas,name = 'input') #Создать получающую информацию, объект потока
+t2 = threading.Thread(target = outdatas, name= 'out') #Создать отправляемое сообщение, объект потока
+t1.start()
+t2.start()
+
+#Ожидание подключения клиента, объект потока
+#t3 = threading.Thread(target = accept(),name = 'accept')
+#t3.start()
+
+t2.join()
+ 
+#выключение
+for client in clients:
+    client.close()
+server.close()
+print ('сервер отключен')
+
+
