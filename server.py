1e0a8ebda86878a2a524a3046ea6efbd92c40a8e
a19641364 <SAlChotchaeva@sberbank.ru>
2021-11-10 22:35:46 +0300

    Чотчаева С, Многопоточный сервер

--- server.py
+++ server.py
@@ -1,20 +1,35 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+UDP_MAX_SIZE = 65535
 
-msg = ''
+def listen(host = '127.0.0.1', port = 3000):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+	s.bind((host, port)) #прослушивание по указанному хосту и порту
+	print(f'Listening at {host}:{port}')
 
-print(msg)
+	members = []
+	while True:
+		msg, addr = s.recvfrom(UDP_MAX_SIZE)
 
-conn.close()
+		if addr not in members:
+			members.append(addr) #добавляем пользователя в список участников
+
+		if not msg:
+			continue
+
+		client_id = addr[1] #в качестве client_id используем порт
+		if msg.decode('ascii') == '__join':
+			print(f'Client {client_id} joined chat')
+			continue
+
+		msg = f'client {client_id}: {msg.decode("ascii")}'
+		for member in members:
+			if member == addr:
+				continue
+			s.sendto(msg.encode('ascii'), member)
+
+if __name__ == "__main__":
+	listen()
+
+
