bc00e57a9dd192bafa71e7af76a92c2304995c44
Кузнецова Анна <90206809+AKPI20@users.noreply.github.com>
2021-11-11 10:59:48 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,40 +1,32 @@
-import socket
 import threading
+import socket
 import time
-from tqdm import tqdm  # python -m pip install tqdm
+import pyprind
 
 ip = input('Введите адрес (Пример: www.fa.ru или 127.0.0.1): ')
 ports = []
-i = 1000
-
-try:
-    def scan(ip, port):
-        global ports
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(.05)
-        try:
-            sock.connect((ip, port))
-            print('Порт', port, 'открыт')
-            ports.append(port)
-            sock.close()
-        except:
-            pass
-
-    def p_port():
-        for port in tqdm(range(i)):
-            if port in ports:
-                continue
-            thread = threading.Thread(target=scan, args=(ip, port))
-            thread.start()
-            time.sleep(.01)
-
-    print('Список открытых портов:', ports)
+i = 2 ** 16 - 1
+bar = pyprind.ProgBar(i)
 
 
-    th2 = threading.Thread(target=p_port, name='open')
-    th2.start()
-    th2.join()
+def scan(ip, port):
+    global ports
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(.5)
+    try:
+        sock.connect((ip, port))
+        ports.append(port)
+        sock.close()
+    except:
+        pass
 
 
-except ValueError:
-    print('Ошибка при вводе данных. Попробуйте ещё раз')
+for port in range(i):
+    if port in ports:
+        continue
+    thread = threading.Thread(target=scan, args=(ip, port))
+    thread.start()
+    bar.update()
+
+
+print('Список открытых портов:', ports)
