9958dc19456fc4230f5f0e44ce63a95c77dec378
Daniil208717 <90206838+Daniil208717@users.noreply.github.com>
2021-11-11 13:20:42 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,15 +1,29 @@
+# coding=utf-8
+import threading
 import socket
-from threading import Thread
 
-N = 2**16 - 1
 
-for port in range(1,100):
-    sock = socket.socket()
-    try:
-        print(port)
-        sock.connect(('127.0.0.1', port))
-        print("Порт", i, "открыт")
-    except:
-        continue
-    finally:
-        sock.close()
+def scanp(port):
+    global r
+    p1 = port * 1024
+    p2 = p1 - 1024
+    for i in range(p2, p1):
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        if i == r - 1:
+            print ("Сканирование завершено!")
+        try:
+            connection = sock.connect(('127.0.0.1', i))
+            print( i, "открыт.")
+            connection.close()
+        except:
+            # print(i)
+            pass
+
+
+s = 4  # Введите число s от 1 до 64.  1 = 1024, 64 = 65536
+r = s * 1024
+print ("Запущено сканирование " + str(r) + " портов")
+for element in range(s + 1):
+    t = threading.Thread(target=scanp, kwargs={'port': element})
+    t.start()
