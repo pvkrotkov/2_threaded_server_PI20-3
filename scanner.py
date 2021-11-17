3cab3b7f643ad7a8f2309a05f4e014023205bd5a
Ivan171777 <90206756+Ivan171777@users.noreply.github.com>
2021-11-17 13:23:44 +0300

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,20 +1,38 @@
 import socket
-import threading
-import pyprind
-N = 2 ** 16 - 1
-ip = input('Введите ip: ')
+from concurrent.futures import ThreadPoolExecutor
+import tqdm
 
-def scan_port(ip, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(0.5)
+def scan_port(host, port, progress_bar, ports):
+    sock = socket.socket() #создаем сокет
+    sock.settimeout(0.3)
     try:
-        sock.connect((ip, port))
-        print('\n',"Порт", port, "открыт")
-        sock.close()
+        sock.connect((host, port)) #пытаемся подключиться к порту
+        ports.append(port)
     except:
         pass
-bar = pyprind.ProgBar(N)
-for i in range(N):
-    potoc = threading.Thread(target=scan_port, args=(ip, i))
-    potoc.start()
-    bar.update()
+    progress_bar.update()
+
+
+host = input('Введите адрес: ')
+start_port = int(input('Введите минимальный порт: '))
+end_port = int(input('Введите максимальный порт: '))
+thread = int(input('Введите количество потоков, которые нужно использовать для сканирования одновременно: '))
+
+
+
+open_ports = [] #список открытых портов
+progress_bar = tqdm.tqdm(total=end_port-start_port+1)
+
+with ThreadPoolExecutor(thread) as executor: #создание исполнительного блока экземпляра ThreadPoolExecutor, который будет быстро очищать потоки после завершения работы
+    futures = []
+    for port in range(start_port, end_port+1):
+        future = executor.submit(scan_port, host, port, progress_bar, open_ports) #возвращает объект future из futures
+        futures.append(future)
+    for future in futures:
+        future.result() #Вызываем для всех отправленных вызовов, чтобы программа не пропустила никаких исключений функции.
+
+progress_bar.close()
+
+
+for port in open_ports:
+    print(f'Port {port} is opened!')
