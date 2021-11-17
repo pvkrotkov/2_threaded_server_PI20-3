efab242e7c71602648f8def9fdbdbd79dc9256a0
Ivan171777 <90206756+Ivan171777@users.noreply.github.com>
2021-11-17 13:24:09 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,17 +1,33 @@
 import socket
 
-sock = socket.socket()
-sock.bind(('', 1777))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+UDP_MAX_SIZE = 65535
 
-msg = ''
+def listen(host = '127.0.0.1', port = 3000):
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-while msg !='exit':
-    data = conn.recv(1024)
-    msg = data.decode()
-    print(msg)
-    conn.send(data)
+	s.bind((host, port)) #прослушивание по указанному хосту и порту
+	print(f'Listening at {host}:{port}')
 
-conn.close()
+	members = []
+	while True:
+		msg, addr = s.recvfrom(UDP_MAX_SIZE)
+
+		if addr not in members:
+			members.append(addr) #добавляем пользователя в список участников
+
+		if not msg:
+			continue
+
+		client_id = addr[1] #в качестве client_id используем порт
+		if msg.decode('ascii') == '__join':
+			print(f'Client {client_id} joined chat')
+			continue
+
+		msg = f'client {client_id}: {msg.decode("ascii")}'
+		for member in members:
+			if member == addr:
+				continue
+			s.sendto(msg.encode('ascii'), member)
+
+if __name__ == "__main__":
+	listen()
