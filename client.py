39394002a7892560c8f0686f81e4187cde486c74
EdemGit <92623671+EdemGit@users.noreply.github.com>
2021-11-18 04:03:14 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,16 +1,39 @@
 import socket
-from time import sleep
+import threading
 
-sock = socket.socket()
-sock.setblocking(1)
-sock.connect(('10.38.165.12', 9090))
 
-#msg = input()
-msg = "Hi!"
-sock.send(msg.encode())
+def post_data():
+    while True:
+        data = input()
+        print()
+        if data == 'quit':
+            break
+        client.send(f'{name}: {data}'.encode('utf-8'))
+        print(f'{name}: {data}')
 
-data = sock.recv(1024)
 
-sock.close()
+def get_data():
+    while True:
+        try:
+            print(client.recv(1024).decode('utf-8'))
+        except ConnectionAbortedError:
+            break
 
-print(data.decode())
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = input('Enter required IP address: ')
+name = input('Enter your name: ')
+
+client.connect((host, 4603))
+print('Connected to the server\nType \'quit\' to close the connection')
+
+thread1 = threading.Thread(target=get_data, name='in')
+thread2 = threading.Thread(target=post_data, name='out')
+
+thread1.start()
+thread2.start()
+
+thread2.join()
+
+print('Stopping')
+client.close()
